801043d3aed3a1fc8179e228a10b892d62bf3bef
Sanjib Ahmad <sanjib@sanjib.org>
2015-04-22 13:06:46 +0600

    Add history button, ViewController and connect them with segue

---
+++ Roshambo/HistoryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HistoryViewController.swift
+//  Roshambo
+//
+//  Created by Sanjib Ahmad on 4/22/15.
+//  Copyright (c) 2015 Object Coder. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
