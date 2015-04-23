e671430ab7888a57c013cb142428703f06cd741c
Sanjib Ahmad <sanjib@sanjib.org>
2015-04-23 12:28:01 +0600

    Put game logic in MatchResult class (MatchResult should be renamed now that it contains game logic also)

--- Roshambo/MatchResult.swift
+++ Roshambo/MatchResult.swift
@@ -8,22 +8,63 @@
 
 import Foundation
 
-class MatchResult: NSObject {
+class MatchResult {
     let humanPlayersChoice: String
     let computerPlayersChoice: String
     var whoWon: WhoWon!
     var whoWonResult: String!
+    var whoWonMessage : String {
+        if whoWon == WhoWon.Computer {
+            return "Computer wins!"
+        } else {
+            return "You win!"
+        }
+    }
     var imageName: String!
     
     init(humanPlayersChoice: String, computerPlayersChoice: String) {
         self.humanPlayersChoice = humanPlayersChoice
         self.computerPlayersChoice = computerPlayersChoice
+        
+        switch humanPlayersChoice {
+        case "rock" where computerPlayersChoice == "paper":
+            paperCoversRock(.Computer)
+        case "rock" where computerPlayersChoice == "scissor":
+            rockCrushesScissors(.Human)
+        case "paper" where computerPlayersChoice == "rock":
+            paperCoversRock(.Human)
+        case "paper" where computerPlayersChoice == "scissor":
+            scissorsCutPaper(.Computer)
+        case "scissor" where computerPlayersChoice == "rock":
+            rockCrushesScissors(.Computer)
+        case "scissor" where computerPlayersChoice == "paper":
+            scissorsCutPaper(.Human)
+        default:
+            whoWon = .Tie
+            whoWonResult = "It's a tie!"
+            imageName = "itsATie"
+        }
     }
-
+    
+    private func paperCoversRock(whoWon: WhoWon) {
+        self.whoWon = whoWon
+        whoWonResult = "Paper covers rock. \(whoWonMessage)"
+        imageName = "PaperCoversRock"
+    }
+    
+    private func rockCrushesScissors(whoWon: WhoWon) {
+        self.whoWon = whoWon
+        whoWonResult = "Rock crushes scissors. \(whoWonMessage)"
+        imageName = "RockCrushesScissors"
+    }
+    
+    private func scissorsCutPaper(whoWon: WhoWon) {
+        self.whoWon = whoWon
+        whoWonResult = "Scissors cut paper. \(whoWonMessage)"
+        imageName = "ScissorsCutPaper"
+    }
 }
 
 enum WhoWon {
-    case Human
-    case Computer
-    case Tie
+    case Human, Computer, Tie
 }
--- Roshambo/ResultsViewController.swift
+++ Roshambo/ResultsViewController.swift
@@ -15,57 +15,12 @@ class ResultsViewController: UIViewController {
     @IBOutlet weak var whoWonLabel: UILabel!
     
     var matchResult: MatchResult!
-    let whoWonEnumToNaturalLanguageTranslation = [WhoWon.Computer: "Computer wins!", WhoWon.Human: "You win!"]
-    
-    private func paperCoversRock(whoWon: WhoWon) {
-        matchResult.whoWon = whoWon
-        matchResult.whoWonResult = "Paper covers rock. \(whoWonEnumToNaturalLanguageTranslation[whoWon]!)"
-        matchResult.imageName = "PaperCoversRock"
-    }
-    
-    private func rockCrushesScissors(whoWon: WhoWon) {
-        matchResult.whoWon = whoWon
-        matchResult.whoWonResult = "Rock crushes scissors. \(whoWonEnumToNaturalLanguageTranslation[whoWon]!)"
-        matchResult.imageName = "RockCrushesScissors"
-    }
-    
-    private func scissorsCutPaper(whoWon: WhoWon) {
-        matchResult.whoWon = whoWon
-        matchResult.whoWonResult = "Scissors cut paper. \(whoWonEnumToNaturalLanguageTranslation[whoWon]!)"
-        matchResult.imageName = "ScissorsCutPaper"
-    }
-    
-    private func playGame() {
-        let humanPlayersChoice = self.matchResult.humanPlayersChoice
-        let computerPlayersChoice = self.matchResult.computerPlayersChoice
-        
-        humanPlayersChoiceLabel.text = "You chose: \(humanPlayersChoice)"
-        computerPlayersChoiceLabel.text = "Computer chose: \(computerPlayersChoice)"
-        
-        switch humanPlayersChoice {
-        case "rock" where computerPlayersChoice == "paper":
-            paperCoversRock(WhoWon.Computer)
-        case "rock" where computerPlayersChoice == "scissor":
-            rockCrushesScissors(WhoWon.Human)
-        case "paper" where computerPlayersChoice == "rock":
-            paperCoversRock(WhoWon.Human)
-        case "paper" where computerPlayersChoice == "scissor":
-            scissorsCutPaper(WhoWon.Computer)
-        case "scissor" where computerPlayersChoice == "rock":
-            rockCrushesScissors(WhoWon.Computer)
-        case "scissor" where computerPlayersChoice == "paper":
-            scissorsCutPaper(WhoWon.Human)
-        default:
-            matchResult.whoWon = WhoWon.Tie
-            matchResult.whoWonResult = "It's a tie!"
-            matchResult.imageName = "itsATie"
-        }
-    }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
-        playGame()
+        humanPlayersChoiceLabel.text = "You chose: \(matchResult.humanPlayersChoice)"
+        computerPlayersChoiceLabel.text = "Computer chose: \(matchResult.computerPlayersChoice)"
         
         var image: UIImage!
         whoWonLabel.text = matchResult.whoWonResult
